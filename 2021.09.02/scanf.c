76658efdc627a28d6ac19e2fc6c50317e0cbb018
tjsdnt1369 <80244800+tjsdnt1369@users.noreply.github.com>
2021-09-02 21:27:10 +0900

    Add files via upload

---
+++ 2021.09.02/scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	char string[20];
+	char c;
+	printf("문자열 입력 :");
+	scanf("%s", string);
+	c = getchar(); 
+	printf("문자 입력 :");
+	scanf("%c", &c);
+	
+	
+	printf("%s\n", string);
+	
+	printf("!!%c!!\n", c);
+	
+	return 0;
+}
